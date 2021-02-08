7f983b0f9c79c0891c696caa3670ae39bd7c2ca0
AndyLu <smallc54@hotmail.com>
2021-02-08 23:45:35 +0800

    refactor: extract method isSameScore and isDeuce

--- lib/src/Tennis.dart
+++ lib/src/Tennis.dart
@@ -4,20 +4,11 @@ class Tennis {
   var scoreLookUp = {0: 'love', 1: 'fifteen', 2: 'thirty', 3: 'forty'};
 
   String score() {
-    if (firstPlayerScoreTimes == 0 && secondPlayerScoreTimes == 0) {
+    if (isSameScore()) {
+      if (isDeuce()) {
+        return 'deuce';
+      }
       return '${scoreLookUp[firstPlayerScoreTimes]}_all';
-    }
-    if (firstPlayerScoreTimes == 1 && secondPlayerScoreTimes == 1) {
-      return '${scoreLookUp[firstPlayerScoreTimes]}_all';
-    }
-    if (firstPlayerScoreTimes == 2 && secondPlayerScoreTimes == 2) {
-      return '${scoreLookUp[firstPlayerScoreTimes]}_all';
-    }
-    if (firstPlayerScoreTimes == 3 && secondPlayerScoreTimes == 3) {
-      return 'deuce';
-    }
-    if (firstPlayerScoreTimes == 4 && secondPlayerScoreTimes == 4) {
-      return 'deuce';
     }
 
     if (firstPlayerScoreTimes >= 1 && secondPlayerScoreTimes >= 0 ||
@@ -38,6 +29,10 @@ class Tennis {
     }
     return null;
   }
+
+  bool isDeuce() => firstPlayerScoreTimes >= 3;
+
+  bool isSameScore() => firstPlayerScoreTimes == secondPlayerScoreTimes;
 
   void firstPlayerScore() {
     firstPlayerScoreTimes++;
